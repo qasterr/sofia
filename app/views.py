a3ff6e5b1b3e224fecc5beb97593f061d296fa80
Muffin <50763425+MuffinLover42@users.noreply.github.com>
2021-11-23 19:25:26 +0100

    Added index page logic

--- app/views.py
+++ app/views.py
@@ -1,11 +1,35 @@
 """Contains all the routes for sofia."""
 
+from pathlib import Path
+from json import loads
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
+
+lessons = Path(Path(__file__).parent.parent / "lessons")
 
 
 @views.route("/")
 def index():
-    """Return the page's index."""
-    return render_template("index.html")
+    """Return the page"s index."""
+    courses = []
+
+    for entry in lessons.glob("*"):
+        if entry.is_dir():
+            lesson_path = Path(entry / "lesson.json")
+
+            if lesson_path.exists():
+                # pylint: disable=invalid-name
+                with open(lesson_path, "r", encoding="utf-8") as f:
+                    course_info = loads(f.read())
+
+                    course_info["location"] = entry.name
+
+                    courses.append(course_info)
+            # Skip if the lesson path starts with `.`
+            elif not entry.name.startswith("."):
+                print(
+                    f"Lesson {entry.name} has no `lesson.json` file. Skipped."
+                )
+
+    return render_template("index.html", courses=courses)
